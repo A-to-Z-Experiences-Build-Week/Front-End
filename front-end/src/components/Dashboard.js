fdebd0df7b758337972b77e78b107d05e52ee8f3
Folasade Agbaje <folasadeagbaje@gmail.com>
2019-09-25 13:15:17 +0100

    Installed react icons

--- front-end/src/components/Dashboard.js
+++ front-end/src/components/Dashboard.js
@@ -1,15 +1,20 @@
 import React from "react";
 import ExperiencePreviewCard from "./ExperiencePreviewCard";
-import { Text, Heading, Button, Flex } from "@chakra-ui/core";
+import { Text, Heading, Button, Flex, Box } from "@chakra-ui/core";
+import { FaPlus } from "react-icons/fa";
 
 function Dashboard(props) {
   return (
     <>
-      <Flex>
-        <Heading>Your Experiences</Heading>
-        <Button>Create Experience</Button>
-      </Flex>
-      <ExperiencePreviewCard />
+      <Box marginX="40px">
+        <Flex justify="space-between">
+          <Heading>Your Experiences</Heading>
+          <Button rightIcon={FaPlus} variantColor="teal" variant="outline">
+            Create Experience
+          </Button>
+        </Flex>
+        <ExperiencePreviewCard />
+      </Box>
     </>
   );
 }
