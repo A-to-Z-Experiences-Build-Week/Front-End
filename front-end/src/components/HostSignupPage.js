f9247cbeb95b83e37dd6fe366f0937261919acfa
Folasade Agbaje <folasadeagbaje@gmail.com>
2019-09-27 00:09:33 +0100

    Added history. push

--- front-end/src/components/HostSignupPage.js
+++ front-end/src/components/HostSignupPage.js
@@ -4,6 +4,7 @@ import { Heading, Box, Flex, Image } from "@chakra-ui/core";
 import HostSignupForm4 from "./HostSignupForm4";
 
 function HostSignupPage(props) {
+  const { history } = props;
   return (
     <>
       <Box>
@@ -11,7 +12,7 @@ function HostSignupPage(props) {
           Sign up Form
         </Heading>
         <Flex justify="space-between" align="center">
-          <HostSignupForm4 onSubmit={values => alert(JSON.stringify(values))} />
+          <HostSignupForm4 onSubmit={values => history.push("/dashboard")} />
           <Image
             src="https://images.unsplash.com/photo-1527707471127-c3ad5ea438d2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2468&q=80"
             width="100%"
